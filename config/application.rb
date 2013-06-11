94f71faee2026348f6c8647cb99f204be9054688
Tim Jarratt <tjarratt@gmail.com>
2013-06-10 17:06:30 -0700

    Don't connect to postgres when precompiling assets
    This is causing a problem with heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module IllegalToaster
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # don't connect to the database when precompiling
+    config.assets.initialize_on_precompile = false
   end
 end
